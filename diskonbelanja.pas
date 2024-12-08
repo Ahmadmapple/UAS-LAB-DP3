00ecfb9ccf8305c443f6ac92ac688da51b34d149
FadlyHudaya711 <fadlyhudaya347@gmail.com>
2024-12-08 19:33:49 +0700

    menambahkan array dan record

--- diskonbelanja.pas
+++ diskonbelanja.pas
@@ -1,2 +1,16 @@
 program Projekdiskonbelanja;
 uses crt;
+
+type
+    TBarang = record
+        nama: string;
+        harga: longint;
+    end;
+
+var
+    barang: array[1..5] of TBarang;
+    keranjang: array[1..100] of integer;
+    jumlahBarang: array[1..100] of integer;
+    pilihan, total, jumlahItem, jumlahBeli: longint;
+    i: integer;
+
